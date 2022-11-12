de8d76b2c8d0054423678126e29456f08b8094b4
kalikayiCode <sunilkalikayiofficial@gmail.com>
2022-11-12 16:40:12 +0530

    watch videos page is created

--- src/App.js
+++ src/App.js
@@ -8,19 +8,21 @@ import Section4 from "./components/Section4/Section4";
 import Section5 from "./components/Section5/Section5";
 import Section6 from "./components/Section6/Section6";
 import Section7 from "./components/Section7/Section7";
+import VideosPage from "./components/videosPage/VideosPage";
 import InternTask from "./InternTask/InternTask";
 
 function App() {
     return(
         <div>
-        <Section1/>
+        <VideosPage/>
+        {/* <Section1/>
         <Section2/>
         <Section3/>
         <Section4/>
         <Section5/>
         <Section6/>
         <Section7/>
-        <Footer/>
+        <Footer/> */}
         </div>
     )
     // return <InternTask/> ;
